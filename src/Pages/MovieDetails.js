c0f4892100579aac97f3c87bc23bef4804f1f80f
Hypex <zaghouane.abdallah@gmail.com>
2023-09-09 15:56:00 +0100

    fixes

--- src/Pages/MovieDetails.js
+++ src/Pages/MovieDetails.js
@@ -7,48 +7,19 @@ import AccountIcon from "../Assets/images/accounticon.png"
 import FireIcon from "../Assets/images/fireicon.png" 
 
 export default function MovieInfos() {
-    const[allMovies, setAllMovies] = React.useState([])
-    const[popularMovies, setPopular] = React.useState([])
     const[similarMovies, setSimilar] = React.useState([])
     const[sim, setSim] = React.useState([true])
     const[details, setDetails] = React.useState([])
     const[backdrop, setBackdrop] = React.useState("")
     const[poster, setPoster] = React.useState("")
+    const[detailsOn, setDetailsOn] = React.useState(false)
+    const[director, setDirector] = React.useState([])
+    const[cast, setCast] = React.useState([])
 
 
-    const {id,page} = useParams()
+    const {id} = useParams()
     const movieId = parseInt(id, 10);
     
-    React.useEffect(() => {
-        const options = {
-          method: 'GET',
-          headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMjZkNzcyZTA2NzExZDQwMzhkMWEyNDkxYzRhMWFmNSIsInN1YiI6IjY0Zjc2NDA5YThiMmNhMDBlMTU4ODk3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yrUE61r8ZMOUB43DZZhc8XZh89-aCelZG5a5LvG_OIQ'
-          }
-        };
-        
-        fetch(`https://api.themoviedb.org/3/trending/all/day?language=en-US&page=${page}`, options)
-          .then(response => response.json())
-          .then(response => setAllMovies(response.results))
-          .catch(err => console.error(err));
-       }, [page]);
-       
-       React.useEffect(() => {
-        const options = {
-          method: 'GET',
-          headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMjZkNzcyZTA2NzExZDQwMzhkMWEyNDkxYzRhMWFmNSIsInN1YiI6IjY0Zjc2NDA5YThiMmNhMDBlMTU4ODk3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yrUE61r8ZMOUB43DZZhc8XZh89-aCelZG5a5LvG_OIQ'
-          }
-        };
-        
-        fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`, options)
-          .then(response => response.json())
-          .then(response => setPopular(response.results))
-          .catch(err => console.error(err));
-      }, []);
-
        React.useEffect(() => {
         const options = {
           method: 'GET',
@@ -62,10 +33,10 @@ export default function MovieInfos() {
           .then(response => response.json())
           .then(response => setSimilar(response.results))
           .catch(err => console.error(err));
-      }, [page]);
+      }, [movieId]);
 
-      const first10 = similarMovies ? similarMovies.slice(0,7) : [];
-      const similarMoviesArray = first10.map(movie => {
+      const first7 = similarMovies ? similarMovies.slice(0,7) : [];
+      const similarMoviesArray = first7.map(movie => {
         return(
           <Movie 
             key = {movie.id}
@@ -132,8 +103,71 @@ export default function MovieInfos() {
           </>
         );
       }) : [];
+
       const rating = parseFloat(details.vote_average).toFixed(1);
-      console.log(details);
+
+      const countriesArray = details.production_countries ? details.production_countries.map((country,index) => {
+        return(
+          <>
+                <p key={index} style={{marginLeft:"10px", opacity:"0.85"}}>{country.name},</p>
+          </>
+        )
+      }) : [];
+
+      const genreDetailArray = details.genres ? details.genres.map((genre,index) => {
+        return(
+          <>
+                <p key={index} style={{marginLeft:"10px", opacity:"0.85"}}>{genre.name},</p>
+          </>
+        )
+      }) : [];
+
+      React.useEffect(() => {
+        const options = {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMjZkNzcyZTA2NzExZDQwMzhkMWEyNDkxYzRhMWFmNSIsInN1YiI6IjY0Zjc2NDA5YThiMmNhMDBlMTU4ODk3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yrUE61r8ZMOUB43DZZhc8XZh89-aCelZG5a5LvG_OIQ'
+          }
+        };
+        
+        fetch(`https://api.themoviedb.org/3/movie/${movieId}/credits?language=en-US`, options)
+          .then(response => response.json())
+          .then((jsonData)=>jsonData.crew.filter(({job})=> job ==='Director'))
+          .then(response => setDirector(response[0]))
+          .catch(err => console.error(err));
+      }, [movieId])
+
+      React.useEffect(() => {
+        const options = {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkMjZkNzcyZTA2NzExZDQwMzhkMWEyNDkxYzRhMWFmNSIsInN1YiI6IjY0Zjc2NDA5YThiMmNhMDBlMTU4ODk3MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yrUE61r8ZMOUB43DZZhc8XZh89-aCelZG5a5LvG_OIQ'
+          }
+        };
+        
+        fetch(`https://api.themoviedb.org/3/movie/${movieId}/credits?language=en-US`, options)
+          .then(response => response.json())
+          .then(response => setCast(response.cast))
+          .catch(err => console.error(err));
+      }, [movieId])
+
+      const cast3 = cast ? cast.slice(0, 3) : [];
+      const castArray = cast3 ? cast3.map((actor,index) => {
+        return(
+          <>
+                <p key={index} style={{marginLeft:"10px", opacity:"0.85"}}>{actor.name},</p>
+          </>
+        )
+      }) : [];
+      const productionArray = details.production_companies ? details.production_companies.map((company,index) => {
+        return(
+          <>
+                <p key={index} style={{marginLeft:"10px", opacity:"0.85"}}>{company.name},</p>
+          </>
+        )
+      }) : [];
     return (
     <div className='Movie--Details'>
     <div className='Upper--Container'>
@@ -163,7 +197,8 @@ export default function MovieInfos() {
             <img src={posterUrl}/>
           </div>
           <div className='Main--Overview'>
-            <p style={{fontSize:"50px"}}>{details.original_title}</p>
+          <p style={{fontSize:"50px"}}>{details.original_title}</p>
+          {!detailsOn && <>
             <div style={{display:"flex"}}>
               {genreArray}
             </div>
@@ -184,16 +219,56 @@ export default function MovieInfos() {
             <p>{rating} / 10</p>
             <p style={{marginLeft:"20px",fontWeight:"400",fontSize:"18px",marginTop:"2px"}}>({details.vote_count})</p>
           </div>
+          </>}
+          </div>
+          {detailsOn && 
+            <div className='details--cont'>
+              <div className='details--element'>
+                <p style={{fontWeight:"700"}}>Type : </p>
+                <p style={{marginLeft:"10px", opacity:"0.85"}}>Movie</p>
+              </div>
+              <div className='details--element'>
+                <p style={{fontWeight:"700"}}>Country : </p>
+                {countriesArray}
+              </div>
+              <div className='details--element'>
+                <p style={{fontWeight:"700"}}>Genre : </p>
+                {genreDetailArray}
+              </div>
+              <div className='details--element'>
+                <p style={{fontWeight:"700"}}>Release Date : </p>
+                <p style={{marginLeft:"10px", opacity:"0.85"}}>{details.release_date}</p>
+              </div>
+              <div className='details--element'>
+                <p style={{fontWeight:"700"}}>Director : </p>
+                <p style={{marginLeft:"10px", opacity:"0.85"}}>{director.name}</p>
+              </div>
+              <div className='details--element'>
+                <p style={{fontWeight:"700"}}>Production : </p>
+                {productionArray}
+              </div>
+              <div className='details--element'>
+                <p style={{fontWeight:"700"}}>Cast : </p>
+                {castArray}
+              </div>
+            </div>
+          }
+        <div className='Lower--Main'>
+          <div className='Lower--Element'>
+            <p onClick={() => setDetailsOn(false)}>OVERVIEW</p>
+            <hr style={{ visibility: detailsOn ? 'hidden' : 'visible' }} />
+          </div>
+          <div className='Lower--Element' style={{marginLeft:"70px"}}>
+            <p onClick={() => setDetailsOn(true)}>DETAILS</p>
+            <hr style={{ visibility: detailsOn ? 'visible' : 'hidden' }} />
           </div>
         </div>
-        <div className='Lower--Main'>
-          
         </div>
       </div>
       <div className='Lower--Container'>
         <h1>Recommendations</h1>
         <div className='Similar--Container'>
-          {similarMoviesArray}
+          {similarMoviesArray.length === 0 ? <h1 style={{marginLeft:"760px",marginTop:"0px"}}>Something went wrong!</h1> : similarMoviesArray}
         </div>
       </div>
     </div>
